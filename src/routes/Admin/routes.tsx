5b5b6020bfd255ccd8f423aa2c853be736f0eac9
imakrp <juliocargoncalves@gmail.com>
2024-10-10 16:33:24 -0300

    message error

---
+++ src/routes/Admin/routes.tsx
@@ -0,0 +1,6 @@
+export const show = "/show";
+export const create = "/create";
+export const edit = "/edit";
+export const list = "/list";
+export const dashboard = "/dashboard";
+export const settings = "/settings";
