8656809e39cc95a7c10a0490ee1c4e78d96983f2
marcoduz <marco.duz@estudante.uffs.edu.br>
2024-10-11 14:34:25 -0300

    Table

--- src/components/Table/index.tsx
+++ src/components/Table/index.tsx
@@ -4,13 +4,15 @@ interface IProps {
   items: string[];
 }
 
-const Table: React.FC<IProps> = ({ items = ["o", "o"] }) => {
+const Table: React.FC<IProps> = ({
+  items = ["Nome", "Categoria", "Descrição", "Quantidade"],
+}) => {
   return (
     <div className="flex-1">
       <div
         className={twMerge(
           "bg-zinc-200 rounded-xl grid py-2 px-3 text-base font-montserrat",
-          items?.length && `grid-cols-[repeat(${items?.length},_1fr)_100px]`
+          items?.length && `grid-cols-${items.length}`
         )}
       >
         {items?.map((key) => (
